966dc49a2437c0771168256395b663f3048a7063
Robert Ferro <70308342+robertferro@users.noreply.github.com>
2021-07-02 10:03:51 -0300

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: sh setup.sh && streamlit run credit_score_app.py
+
---
+++ teste.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+
+
+st.title('Multiplica números')
+
+a=st.selectbox('digite o primeiro número: ',np.arange(1,11))
+b=st.selectbox('digite o segundo número: ',np.arange(1,11))
+c=st.write('o resultado é : {}'.format(a*b))
+# a=int(input())
+# b=int(input('digite o segundo número: '))
+
